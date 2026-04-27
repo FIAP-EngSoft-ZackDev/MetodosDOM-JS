1db2da7ffc2657d5432c7e30cff0bd9cd6a59943
Isac <contatoisacn@gmail.com>
2026-04-27 09:57:42 -0300

    feat: método DOM aplicado

--- src/js/script.js
+++ src/js/script.js
@@ -35,3 +35,35 @@ const desconto = 0.15; //15%
 const precoFinal = precoProduto * (1 - desconto);
 console.log(precoFinal);
 console.log(`R$ ${precoFinal.toFixed(2)}`);
+
+/* DOM ( DOCUMENT OBJECT MODEL) MODELO DE DOCUMENTO
+PERMITE O JAVASCRIPT CONVERSAR COM O NAVEGADOR
+PARA MANIPULAR O HTML E CSS. */
+
+const titulo = document.getElementById("titulo");
+titulo.innerText = "Dom transformando o texto";
+
+titulo.style.color = "blue";
+
+const produtos =[
+    {nome:"Teclado Gamer", preco:200, emPromocao:true},
+    {nome:"Mouse Gamer", preco:150, emPromocao:false},
+    {nome:"Monitor", preco:1300, emPromocao:true},
+    {nome:"Pad Mouse", preco:50, emPromocao:false}
+];
+
+console.log(produtos);
+const container = document.getElementById("lista-produtos");
+
+function produtosCard(lista){
+    const hmtlProdutos = lista.map(item => `
+        <div class="card">
+            <h3>${item.nome}</h3>
+            <p>Preço R$ ${item.preco}</p>
+            ${item.emPromocao}? '<span>Promoção</span>': ''}
+        </div>
+        `).join(""); //transforma o array e, uma unica string
+
+        container.innerHTML=hmtlProdutos;
+}
+produtosCard();
